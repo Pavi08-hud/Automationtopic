ae77f044413632700937d5897987e2775a2a5fdc
Pavi08-hud <pavithra.raj08011999@gmail.com>
2026-07-09 13:46:31 +0530

    Fruit name changed

--- Banana.java
+++ Banana.java
@@ -2,7 +2,7 @@ public class Banana {
 
  public static void main(String[] args) {
 	 
-	 System.out.println("the the fruit name as Banana");
+	 System.out.println("the the fruit name as now has changed to Orange");
 	
 }
 	
